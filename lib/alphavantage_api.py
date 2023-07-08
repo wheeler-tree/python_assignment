ad3dd4d981ba631d37e9f152cb9b90ff22a2a669
xu.mingwei <huimingway@foxmail.com>
2023-07-08 13:54:06 +0900

    query raw data

---
+++ lib/alphavantage_api.py
@@ -0,0 +1,16 @@
+from typing import Optional
+import requests
+
+
+class AlphaVantageAPI:
+    DEFAULT_FUNC = "TIME_SERIES_DAILY_ADJUSTED"
+    DEPRECATION_LIMIT_DAYS = 14
+    FUNC_DATA_KEY = {"TIME_SERIES_DAILY_ADJUSTED": "Time Series (Daily)"}
+
+    def __init__(self, api_key: str, func: Optional[str] = DEFAULT_FUNC):
+        self.api_key = api_key
+        self.func = func
+    
+    def get_daily_data_json(self, symbol_code: str) -> str:
+        url = f'https://www.alphavantage.co/query?function={self.func}&datatype=json&symbol={symbol_code}&outputSize=compact&apikey={self.api_key}'
+        return requests.get(url).json()
